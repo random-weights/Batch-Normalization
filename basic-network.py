082e9c7bc3244883780d55b58a80978262b2878f
thubatilakshmi <thubatilakshmi@gmail.com>
2018-01-15 19:25:04 -0500

    batch_normalization
    implemented

--- basic-network.py
+++ basic-network.py
@@ -36,26 +36,28 @@ alpha = tf.placeholder('float')
 
 weights = [0]*5
 biases = [0]*5
-
+scales = [0]*5
+offsets = [0]*5
 ls_dims = [784,256,128,64,32,10]
 
-#initialize all weights and biases
 a = x
+# initialize all weights and biases
 for i in range(5):
     weights[i] = tf.Variable(tf.random_normal(shape = [ls_dims[i],ls_dims[i+1]],mean = 0.0, stddev=0.01))
-    biases[i] = tf.Variable(tf.constant(1.0,shape = [ls_dims[i+1]]))
-    z = tf.matmul(a,weights[i]) + biases[i]
+    #biases[i] = tf.Variable(tf.constant(1.0,shape = [ls_dims[i+1]]))
+    scales[i] = tf.Variable(tf.ones(shape = [ls_dims[i]]))
+    offsets[i] = tf.Variable(tf.zeros(shape = [ls_dims[i]]))
+    a_mean,a_var = tf.nn.moments(a,[0])
+    a_norm = tf.nn.batch_normalization(a,a_mean,a_var,offsets[i],scales[i],variance_epsilon=1e-6)
+    z = tf.matmul(a_norm,weights[i])
     a = tf.nn.relu(z)
 
 y_pred = tf.nn.softmax(logits = z)
 _,acc = tf.metrics.accuracy(tf.argmax(y_pred,axis = 1),tf.argmax(y_actual,axis = 1))
-norm = 0
-for w in weights:
-    norm += tf.norm(w,ord= 2,axis = None)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = z,labels = y_actual))+norm
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = z,labels = y_actual))
 
-opt = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+opt = tf.train.AdamOptimizer(learning_rate=0.1)
 train = opt.minimize(loss)
 
 epochs = 10000
@@ -66,22 +68,15 @@ with tf.Session() as sess:
     train_data = Data()
     train_data.setDataPaths(x_path = "data/x_train.csv",y_path="data/y_train.csv")
     train_data.getAllData()
-    ls_fname = ["basic_norm_run_"+str(i)+".csv" for i in range(1,6)]
-    ls_fh = [open(file,'a') for file in ls_fname]
-    ls_alpha = [0.001,0.005,0.01,0.05,0.1]
-    for a in range(5):
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.local_variables_initializer())
-        print("----------------------------Run: ",a)
-        for i in range(epochs):
-            train_data.getRandomdata(batch_size)
-            x_batch = train_data.x_batch
-            y_batch = train_data.y_batch
-            feed_dict = {x: x_batch,y_actual:y_batch,alpha:ls_alpha[a]}
-            _,accuracy = sess.run([train,acc],feed_dict)
-            final_str = str(i) + "," + str(accuracy * 100)
-            print("Epoch: ",i,"\tAccuracy: ",accuracy*100)
-            print(final_str,file = ls_fh[a])
-
-    for handler in ls_fh:
-        handler.close()
+    fh = open("R_adam.csv",'a')
+    for i in range(epochs):
+        train_data.getRandomdata(batch_size)
+        x_batch = train_data.x_batch
+        y_batch = train_data.y_batch
+        feed_dict = {x: x_batch,y_actual:y_batch}
+        _,accuracy = sess.run([train,acc],feed_dict)
+        print("Epoch: ",i,"\tAccuracy: ",accuracy*100)
+        if i%100 == 0:
+            final_str = str(i)+","+str(accuracy*100)
+            print(final_str,file = fh)
+    fh.close()
